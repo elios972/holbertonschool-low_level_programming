3c86d6a4a58589a09393aea9ef61d50fed227280
elios972 <elios.b972@gmail.com>
2024-10-24 12:01:52 +0000

    correction

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	char alphabet = 'a', ALPHABET = 'A';
+	
+	while (alphabet <= 'z')
+	{
+                putchar(alphabet);
+                alphabet++;
+		if (alphabet = 'z')
+			while (ALPHABET <= 'Z')
+			putchar(ALPHABET);
+                	ALPHABET++;
+	}
+        putchar('\n');
+return (0);
+}
---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - echo les chiffres de 0 a 9
+ * Return: always 0 (SUCESS)
+ */
+
+int main(void)
+{
+        int n;
+
+        for (n = 0; n < 10; n++)
+		putchar('%i\n', n);
+return (0);
+}
